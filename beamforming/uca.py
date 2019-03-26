acc8b6d998d727014ffe3f2a1b47b082911fce9f
Bagle <s09755064@gmail.com>
2019-03-26 10:34:54 +0800

    diameter to radius && mvdr version 0 -- bagle

--- beamforming/uca.py
+++ beamforming/uca.py
@@ -7,15 +7,15 @@
 ##  DATE  : 12th Feb, 2019
 ##
 ## *****************************************************
-##  FILE        : uca.py 
-##  DESCRIPTION : 
-##                  
+##  FILE        : uca.py
+##  DESCRIPTION :
+##
 ##
 #
 
 import collections
-import threading 
-import numpy as np 
+import threading
+import numpy as np
 import pyaudio
 import os
 import logging
@@ -33,7 +33,7 @@ except:
 from respeaker.vad import vad
 
 logger              = logging.getLogger('uca')
-collecting_audio    = os.getenv('COLLECTING_AUDIO', 'no') 
+collecting_audio    = os.getenv('COLLECTING_AUDIO', 'no')
 
 
 class UCA(object):
@@ -43,20 +43,20 @@ class UCA(object):
     SOUND_SPEED = 343.2
     """
     UCA (Uniform Circular Array)
-    
+
     Design Based on Respeaker 7 mics array architecture
     """
     def __init__(self, fs=16000, nframes=2000, radius=0.032, num_mics=6, quit_event=None, name='respeaker-7'):
-        self.radius     = radius 
+        self.radius     = radius
         self.fs         = fs
-        self.nframes    = nframes 
+        self.nframes    = nframes
         self.nchannels  = (num_mics + 2 if name == 'respeaker-7' else num_mic)
         self.num_mics   = num_mics
         self.max_delay  = radius * 2 / UCA.SOUND_SPEED
         self.delays     = None
 
         self.pyaudio_instance = pyaudio.PyAudio()
-        
+
         self.device_idx = None
         for i in range(self.pyaudio_instance.get_device_count()):
             dev  = self.pyaudio_instance.get_device_info_by_index(i)
@@ -77,7 +77,7 @@ class UCA(object):
             channels    = self.nchannels,
             rate        = self.fs,
             frames_per_buffer   = int(self.nframes),
-            stream_callback     = self._callback, 
+            stream_callback     = self._callback,
             input_device_index  = self.device_idx
         )
 
@@ -107,19 +107,20 @@ class UCA(object):
 
         self.tdoa_matrix -= np.ones((len(mic_theta)-1, 2)) \
                              * np.array([np.cos(mic_theta[0]), np.sin(mic_theta[0])])
-
+#################### wired #######################################
         self.tdoa_measures  = np.ones(((len(mic_theta)-1, ))) \
-                                * UCA.SOUND_SPEED / (self.radius*2)
+                                * UCA.SOUND_SPEED / (self.radius) 		# diameter => radius
+#################### wired #######################################
 
     def wakeup(self, keyword=None):
         self.decoder.end_utt()
-        self.decoder.start_utt()
+        self.decoder.start_iutt()
 
-        # flag up detecting 
+        # flag up detecting
         self.status |= UCA.detecting_mask
 
         # clear detecting queue
-        self.detect_history.clear()    
+        self.detect_history.clear()
         self.detect_queue.queue.clear()
 
         self.stream.start_stream()
@@ -131,9 +132,9 @@ class UCA(object):
                 logger.info('Too many delays, {0} in queue'.format(self.detect_queue.qsize()))
             elif self.detect_queue.empty():
                 continue
-            
+
             data = self.detect_queue.get()
-            
+
             self.detect_history.append(data)
 
             self.decoder.process_raw(data, False, False)
@@ -144,7 +145,7 @@ class UCA(object):
 
                 if collecting_audio != 'no':
                     logger.debug(collecting_audio)
-                    # save detect_history as wave ? 
+                    # save detect_history as wave ?
                 # clear history
                 self.detect_history.clear()
                 if keyword:
@@ -160,7 +161,7 @@ class UCA(object):
                     break
 
         # flag down detecting
-        self.status &= ~UCA.detecting_mask 
+        self.status &= ~UCA.detecting_mask
         self.stop()
         return result
 
@@ -171,7 +172,7 @@ class UCA(object):
 
         tau = [0] * MIC_GROUP_N
 
-        # estimate each group of delay 
+        # estimate each group of delay
         for i, v in enumerate(MIC_GROUP):
             tau[i] = gcc_phat(buf[v[0]::8], buf[v[1]::8], fs=self.fs, max_tau=self.max_delay, interp=10)
 
@@ -180,12 +181,16 @@ class UCA(object):
 
         # least square solution of (cos, sin)
         sol = np.linalg.pinv(self.tdoa_matrix).dot( \
-              (self.tdoa_measures * np.array(tau)).reshape(MIC_GROUP_N, 1)) 
+              (self.tdoa_measures * np.array(tau)).reshape(MIC_GROUP_N, 1))
 
+############################################
+        phi_in_rad = min( sol[1] / math.sin(math.atan2(sol[1],sol[0]) ), 1 )
+        phi = 90 - np.rad2deg( math.asin(phi_in_rad) ) # phi in degree
         # found out theta
         # another 180.0 for positive value, 30.0 for respeaker architecture
-        return ((math.atan2(sol[1], sol[0])/np.pi*180.0 + 210.0) % 360
-                , [0] + tau) 
+        return ([(math.atan2(sol[1], sol[0])/np.pi*180.0 + 210.0) % 360
+                , phi], [0] + tau)
+#############################################
 
     def listen(self, duration=9, timeout=3):
         vad.reset()
@@ -197,9 +202,9 @@ class UCA(object):
         self.listen_queue.queue.clear()
         self.status |= UCA.listening_mask
         self.start()
-        
+
         logger.info('Start Listening')
-        
+
         def _listen():
             """
             Generator for input signals
@@ -222,7 +227,7 @@ class UCA(object):
     def stop(self):
         if not self.status and self.stream.is_active():
             self.stream.stop_stream()
-    
+
     def close(self):
         self.quit()
         self.stream.close()
@@ -235,53 +240,86 @@ class UCA(object):
 
     def beamforming(self, chunks):
         delays = [0.0] * (self.num_mics-1)
-      
-        enhanced_speech = [] 
+
+        enhanced_speech = []
         for chunk in chunks:
             # decode from binary stream
             raw_sigs = np.fromstring(chunk, dtype='int16')
-			# casting int16 to double floating number 
+
+			# casting int16 to double floating number
 			raw_sigs = raw_sigs / (2**15)
 
-			## TODO... expand gcc_phat and DOA function in following
-			
             # tdoa & doa estimation based on planar wavefront
             direction, delays = self.DOA(raw_sigs)
 
-            # setting led && logger info
-            pixel_ring.set_direction(direction)
-            logger.debug('@ {:.2f}, delays = {}'.format(direction, np.array(delays)*self.fs))
+			## TODO... expand gcc_phat and DOA function in following
+            # UCA geometry
+            mic_theta = np.arange(np.pi/6, 2*np.pi, np.pi/3)
+            GD_matrix = np.ones(3,6)*np.array([np.cos(mic_theta), np.sin(mic_theta)], np.zeros(6) )# 3 x 6
+            # rfft
+            for i in range(1,7) # 1~6
+                tf_sigs[i-1] = raw_sigs[i::8]
+
+            len_of_sig= len(tf_sigs[0])
+            tf_sigs = np.fft.rfft(tf_sigs, len_of_sig)
+
+                        # setting led && logger info
+            pixel_ring.set_direction(direction[0])
+
+##################################################
+            logger.debug('@ {:.2f} @ {:.2f}, delays = {}'.format(direction[0], direction[1], np.array(delays)*self.fs))
+            # direction[0] is horizintal angle ; direction[1] is elevation angle
+###################################################
 
 			## TODO...
             ## *************  apply MVDR beamformer  ****************
-			
+            # frq
+            kappa = np.array([np.cos(np.deg2rad(direction[0]))*np.sin(np.deg2rad(90-direction[1])), np.sin(np.deg2rad(direction[0]))*np.sin(np.deg2rad(90-direction[1])), np,cos(np.deg2rad(90-direction[1]))] )
+            df = self.fs/len_of_sig
+            frq = arange(0, (self.fs/2) + df, df)
+            k = 2*np.pi*frq/343.2
+            # create Rxx
+            Rxx = np.zeros((6,6,len(frq)))
+            for i in range(1, 7)# 6 microphones
+                for j in range(1,7)
+                    s_Rxx = np.abs(sum(GD_matrix[:,i-1]-GD_matrix[:,j-1], 2))/np.pi*k
+                    Rxx[i-1][j-1][:]=np.sin(s_Rxx)/(s_Rxx)
+
+            # scan frq
+            for i in range(1,len(frq)+1)
+                A = math.exp(1j*np.dot(k[i-1],kappa*GD_matrix)) # 1x6
+                W = np.dot((np.linalg.inv( Rxx[:][:][i-1] + 0.00001)).T.conj(), np.linalg.inv( Rxx[:][:][i-1] + 0.00001))
+                source_half[i-1] = np.dot((W[:,i-1]).T.conj() , tf_sigs[:,i-1] )
+
+            enhanced_speech.append(np.fft.irfft(source_half, len_of_sig))
+
             ## # *************  apply DAS beamformer  ****************
             ## int_delays = (np.array(delays)*self.fs).astype('int16')
             ## int_delays -= int(np.min(int_delays))
             ## max_delays = np.max(int_delays);
 
-            ## toAdd = np.zeros((raw_sigs.size//8 + max_delays, 
+            ## toAdd = np.zeros((raw_sigs.size//8 + max_delays,
             ##                   self.num_mics), dtype='int16')
             ## # manupilate integer delays
             ## for i in range(self.num_mics):
             ##     # 1. Padding zero in the front and back
             ##     # 2. shift 2 bits (devide by 4)
             ##     toAdd[:, i] = np.concatenate((np.zeros(int_delays[i], dtype='int16'),
-            ##                                    raw_sigs[i+1::8] >> 2, 
-            ##                                    np.zeros(max_delays-int_delays[i], dtype='int16')), 
+            ##                                    raw_sigs[i+1::8] >> 2,
+            ##                                    np.zeros(max_delays-int_delays[i], dtype='int16')),
             ##                                   axis=0)
-
-            # add them together 
-            enhanced_speech.append(np.sum(toAdd, axis=1, dtype='int16'))
-            # *************************************************
+			## 
+            ## # add them together
+            ## enhanced_speech.append(np.sum(toAdd, axis=1, dtype='int16'))
+            ## # *************************************************
 
         return np.concatenate(enhanced_speech, axis=0)
 
     def _callback(self, in_data, frame_count, time_info, status):
         """
         Pyaudio callback function
-        """ 
-        # decode bytes stream 
+        """
+        # decode bytes stream
         mulChans = np.fromstring(in_data, dtype='int16')
         mono     = mulChans[7::8].tostring()
 
@@ -297,7 +335,7 @@ class UCA(object):
                 if not self.active:
                     for d in self.listen_history:
                         self.listen_queue.put(d)
-                        self.listen_countdown[0] -= 1 # count down timeout 
+                        self.listen_countdown[0] -= 1 # count down timeout
                     self.listen_history.clear()
 
                 self.listen_queue.put(in_data)
@@ -308,7 +346,7 @@ class UCA(object):
                 else:
                     self.listen_history.append(in_data)
                 self.listen_countdown[1] -= 1         # coutn down listening time
-            
+
             if self.listen_countdown[0] <= 0 or self.listen_countdown[1] <= 0:
                 self.listen_queue.put('')
                 self.status &= ~self.listening_mask
@@ -318,7 +356,7 @@ class UCA(object):
 
         return None, pyaudio.paContinue
 
-        
+
 
 
     @staticmethod
@@ -326,11 +364,11 @@ class UCA(object):
         from pocketsphinx.pocketsphinx import Decoder
 
         path              = os.path.dirname(os.path.realpath(__file__))
-        pocketsphinx_dir = os.getenv('POCKETSPHINX_DATA', 
-                                os.path.join(path, 'assets', 'pocketsphinx-data')) 
+        pocketsphinx_dir = os.getenv('POCKETSPHINX_DATA',
+                                os.path.join(path, 'assets', 'pocketsphinx-data'))
         hmm = os.getenv('POCKETSPHINX_HMM', os.path.join(pocketsphinx_dir, 'hmm'))
         dic = os.getenv('POCKETSPHINX_DIC', os.path.join(pocketsphinx_dir, 'dictionary.txt'))
-        kws = os.getenv('POCKETSPHINX_KWS', os.path.join(pocketsphinx_dir, 'keywords.txt')) 
+        kws = os.getenv('POCKETSPHINX_KWS', os.path.join(pocketsphinx_dir, 'keywords.txt'))
 
         config = Decoder.default_config()
         config.set_string('-hmm', hmm)
@@ -340,7 +378,7 @@ class UCA(object):
         config.set_int('-nfft', 512)
         config.set_float('-vad_threshold', 2.7)
         config.set_string('-logfn', os.devnull)
-        
+
         return Decoder(config)
 
 
@@ -351,7 +389,7 @@ def task(quit_event):
 
     uca = UCA(fs=16000, nframes=2000, radius=0.032, num_mics=6, \
                 quit_event=quit_event, name='respeaker-7')
-    
+
     while not quit_event.is_set():
         if uca.wakeup('bagel'):
             print('Wake up')
@@ -360,10 +398,10 @@ def task(quit_event):
             uca.beamforming(chunks)
 
     uca.close()
-         
+
 def main():
     import time
-    
+
     logging.basicConfig(level=logging.DEBUG)
 
     q = threading.Event()
