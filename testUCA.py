42ead39061344e055c4e6a5bf9ec95c9551ade2a
Tsung-Han Brian Lee <bobolee1239@gmail.com>
2019-04-02 14:40:19 +0800

    test uca -- brian

---
+++ testUCA.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from beamforming.uca import UCA
+import logging
+import threading 
+import time
+import boto3
+import numpy as np 
+from scipy import signal
+
+import sounddevice as sd
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
+    # setup UCA
+    q = threading.Event()
+    uca = UCA(fs=16000, nframes=2000, radius=0.032, num_mics=6, \
+                quit_event=q, name='respeaker-7')
+
+    enhanced = None
+
+    isFailed = False
+    while not q.is_set():
+        try:
+            if uca.wakeup('hello amber'):
+                print('Wake up')
+                chunks = uca.listen(duration=1, timeout=1)
+                enhanced = uca.beamforming(chunks)
+        except KeyboardInterrupt:
+            print('Quit')
+            q.set()
+            break
+    uca.close()
+
